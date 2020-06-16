220464134f0feafc02339d5daee4555aba6bf195
QuanKaoYang <ishida.goldenbridge2002@gmail.com>
2020-06-16 17:17:26 +0900

    publish

---
+++ blog/.vuepress/components/wordSkill.vue
@@ -0,0 +1,12 @@
+<template>
+    <iframe style="width:120px;height:240px;" marginwidth="0" marginheight="0" scrolling="no" frameborder="0" src="https://rcm-fe.amazon-adsystem.com/e/cm?ref=qf_sp_asin_til&t=goldenbridg09-22&m=amazon&o=9&p=8&l=as1&IS1=1&detail=1&asins=4758921881&linkId=3b894a8c94dc24bd2a303b67774b71b6&bc1=000000&lt1=_blank&fc1=333333&lc1=0066c0&bg1=ffffff&f=ifr">
+    </iframe>
+</template>
+
+<script>
+module.exports = {
+}
+</script>
+
+<style>
+</style>
