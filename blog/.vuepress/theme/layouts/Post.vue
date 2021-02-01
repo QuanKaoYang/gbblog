d8d70d1fedf60e09f722941a357b953b47ea2539
QuanKaoYang <ishida.goldenbridge2002@gmail.com>
2021-02-01 16:04:58 +0900

    printall redirect

--- blog/.vuepress/theme/layouts/Post.vue
+++ blog/.vuepress/theme/layouts/Post.vue
@@ -31,7 +31,7 @@ const redirectList = {
   '/pg/2020/01/03/gci-cheatsheet/': 'https://catovis.com/archives/409',
   '/pg/2020/01/04/moving-file/': 'https://catovis.com/archives/417',
   '/pg/2020/01/01/rni-cheatsheet/': 'https://catovis.com/archives/419',
-  '/pg/2020/01/06/printall/': 'https://catovis.com/archives/430',
+  '/pg/2020/01/01/printall/': 'https://catovis.com/archives/430',
   '/pg/2020/03/06/padleft/': 'https://catovis.com/archives/432',
 }
 
